9f37ac98b7894a59e7c1d5b794ca1d5c449c4327
Terrence <teelt@ymail.com>
2023-04-20 23:57:11 -0400

    working api with discription "failure"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,119 @@
 import React,{Component} from 'react';
+import Clarifai from 'clarifai';
+import ParticlesBg from 'particles-bg';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
 
+const setupClarifai = (imageUrl) =>{
+  // Your PAT (Personal Access Token) can be found in the portal under Authentification
+  const PAT = 'edfa56c0f339486185a852d40371e638';
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = 'concept';       
+  const APP_ID = 'my-first-application';
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = 'face-detection';   
+  const IMAGE_URL = imageUrl ;
+
+ const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+});
+
+const requestOptions = {
+  method: 'POST',
+  headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+  },
+  body: raw
+};
+return requestOptions
+}
+
+ 
+ 
+
+ 
+
+  
+
+ 
+
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      input: '',
+    }
+
+  }
+
+  onInputChange = (event) =>{
+    this.setState({input: event.target.value})
+
+  }
+
+
+  onButtonsubmit = () => {
+   this.setState({imageUrl: this.state.input});   
+   
+    fetch("https://api.clarifai.com/v2/models/" + 'face-detection' + "/outputs", setupClarifai(this.state.input))
+      .then(response => response.json())
+      .then (response =>{
+        console.log('hi',response)
+        if(response){
+          fetch('http://localhost:3000/image',{
+            method: 'put',
+            headers:{'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user,{ entries: count}))
+          })
+
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+      
+      
+      
+  
+  }
+
+
   render(){
   return (
     <div className="App">
+       <ParticlesBg type="cobweb" bg={true} />
       <Navigation/>
       <Logo/>
-      <ImageLinkForm/>
-      {/* <FaceRecognition/> */}
+      <Rank/>
+      <ImageLinkForm 
+      onInputChange={this.onInputChange}
+       onButtonSubmit={this.onButtonsubmit}
+       />
+       { <FaceRecognition imageUrl = {this.state.imageUrl}/>  }
     </div>
   );
 }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,14 +5,16 @@ import './ImageLinkForm.css';
 
 
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return (
         <div >
             <p className='f3'>{'This will detect faces in your pictures'}</p>
             <div className='center'>
                 <div className='form pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='tex' />
-                    <button className='w-30 grow f4 link ph3 dib white bg-light-purple '>Dectect</button>
+                    <input className='f4 pa2 w-70 center' type='tex'onChange={onInputChange} />
+                    <button className='w-30 grow f4 link ph3 dib white bg-light-purple '
+                    onClick={onButtonSubmit}
+                    >Dectect</button>
                 </div>
             </div>
         </div>
